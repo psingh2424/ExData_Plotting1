2ae9b1f87a55e0807f2620e1145985360256cd3c
psingh2424 <psingh2424@gmail.com>
2017-02-12 16:16:19 -0600

    Delete plot 1.R

--- plot 1.R
+++
@@ -1,9 +0,0 @@
-setwd("C:/Users/psingh2424/Desktop/R")
-my_data <- read.csv("household_power_consumption.csv")
-my_data$Date <- as.Date(my_data$Date, format = "%d/%m/%y")
-my_data$Global_active_power <- as.numeric(my_data$Global_active_power)
-hist(my_data$Global_active_power, main = "Global Active Power", col = "red", xlab = "Global Active Power (kilowatts)")
-
-png(filename="C:/Users/psingh2424/Desktop/R/plot1.png", width = 480, height = 480) 
-hist(my_data$Global_active_power, main = "Global Active Power", col = "red", xlab = "Global Active Power (kilowatts)")
-dev.off()
