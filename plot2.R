c1f113e0a49ab68fd7c31ba4d3d1bd90c93b4349
psingh2424 <psingh2424@gmail.com>
2017-02-12 16:11:26 -0600

    Delete plot2.R

--- plot2.R
+++
@@ -1,8 +0,0 @@
-# Beginning of Plot 2
-setwd("C:/Users/psingh2424/Desktop/R")
-my_data <- read.csv("household_power_consumption.csv")
-plot(as.POSIXct(paste(my_data$Date, my_data$Time), format="%d/%m/%Y %H:%M:%S"), my_data$Sub_metering_1, type="l", ylab = "Global Active Power (kilowats)", xlab = "")
-
-png(filename="C:/Users/psingh2424/Desktop/R/plot2.png") 
-plot(as.POSIXct(paste(my_data$Date, my_data$Time), format="%d/%m/%Y %H:%M:%S"), my_data$Sub_metering_1, type="l", ylab = "Global Active Power (kilowats)", xlab = "")
-dev.off()
